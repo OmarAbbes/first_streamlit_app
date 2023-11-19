02ab6cb88220895ac1c8f76135ef41e8431bc553
OmarAbbes <149977537+OmarAbbes@users.noreply.github.com>
2023-11-19 22:26:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,10 @@ fruitevice_responses = requests.get ("https://fruityvice.com/api/fruit/" + fruit
 
 fruityvice_normalized = pandas.json_normalize(fruitevice_responses.json())
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
